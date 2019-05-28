416e3a27820b5b7330a3c5beea0dd7e8b8382b8e
Eduardo Rojas <edurojas.si@outlook.com>
2019-05-27 21:36:34 -0300

    commit de atualização 27_05_2019
    commit de atualização 27_05_2019

---
+++ Estudo_capitulo_1/index.js
@@ -0,0 +1,25 @@
+let express = require ('express')
+let appCap01 = express()
+
+appCap01.set('view engine', 'ejs')
+appCap01.use(express.static('public'))
+
+appCap01.get('/', (req, res)=>{
+    res.render('main')
+})
+
+appCap01.get('/ex01', (req, res)=>{
+    res.render('ex01')
+})
+
+appCap01.get('/ex02', (req, res)=>{
+    res.render('ex02')
+})
+
+appCap01.listen(4000, (error)=>{
+    if(error){
+        console.log('deu merda!')
+    }else{
+        console.log('deu bom')
+    }
+})
