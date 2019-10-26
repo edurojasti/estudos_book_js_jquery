4edd6abdfc75098f551e44d1bbb822bea05c2b0c
Eduardo Rojas <edurojas.si@outlook.com>
2019-10-26 12:43:40 -0300

    atl

--- Estudo_capitulo_6/index.js
+++ Estudo_capitulo_6/index.js
@@ -1,18 +1,18 @@
-let express = require('express')
-let appCap06 = express()
+const express = require('express')
+const appCap06 = express()
 
-appCap06.set('view engine', 'ejs')
-appCap06.use(express.static('public')) //configurando rotas de arquivos css, img, etc.
+appCap06.set('view engine', 'ejs');
+appCap06.use(express.static('public'));//configurando rotas de arquivos css, img, etc.
 
 appCap06.get('/', (req, res)=>{
-    res.render('cap06')                // renderiza a pagina principal do capitulo
+    res.render('cap06');                // renderiza a pagina principal do capitulo
 })
 
 appCap06.get('/ex01_cap06',(req, res)=>{
-    res.render('ex01_cap06')
+    res.render('ex01_cap06');
 })
 
 appCap06.listen(5500, (error)=>{
-    if(error){console.log('sistema Offline!')}
-    else{console.log('Sistema Online!')}
+    if(error){console.log('sistema Offline!');}
+    else{console.log('Sistema Online!');}
 })
